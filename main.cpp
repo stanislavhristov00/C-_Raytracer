4f89ec8009008ec4c7bdefbb4b3bc28aa55c30b1
Stanislav Hristov <stanislavhristov00@abv.bg>
2024-03-21 21:26:25 +0200

    Generating a basic image

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int j = 0; j < imageHeight; j++) {
+        for (int i = 0; i < imageWidth; i++) {
+            auto r = double(i) / (imageHeight - 1);
+            auto g = double(j) / (imageWidth - 1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
